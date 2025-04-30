ec3a6bcb690ee1baec466b2116c1301ba7e86f93
kwrona05 <kwrona05@gmail.com>
2025-04-30 08:27:47 +0200

    Create bilingual header bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,37 @@
+import { useTranslation } from "react-i18next";
+import "../App.css";
+
 const Header = () => {
-  <header>
-    <div>
-      <h1>Tech 4 You</h1>
-    </div>
-    <div>
-      <div>Strona główna</div>
-      <div>Oferta</div>
-      <div>Portfolio</div>
-      <div>O mnie</div>
-      <div>Kontakt</div>
-    </div>
-  </header>;
+  const { t, i18n } = useTranslation();
+  const isEnglish = i18n.language === "en";
+
+  const toggleLanguage = () => {
+    i18n.changeLanguage(isEnglish ? "pl" : "en");
+  };
+
+  return (
+    <header>
+      <div>
+        <h1>Tech 4 You</h1>
+      </div>
+      <div>
+        <div>{t("home-page")}</div>
+        <div>{t("offer")}</div>
+        <div>{t("portfolio")}</div>
+        <div>{t("about")}</div>
+        <div>{t("contact")}</div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isEnglish}
+            onChange={toggleLanguage}
+          />
+          <span className="slider">
+            <span className="flag">{isEnglish ? "🇬🇧" : "🇵🇱"}</span>
+          </span>
+        </label>
+      </div>
+    </header>
+  );
 };
 export default Header;
