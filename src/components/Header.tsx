eaa54ac1f0fc89416bec95caa5745ca920104f7b
kwrona05 <kwrona05@gmail.com>
2025-04-30 07:48:29 +0200

    Start creating Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  <header>
+    <div>
+      <h1>Tech 4 You</h1>
+    </div>
+    <div>
+      <div>Strona główna</div>
+      <div>Oferta</div>
+      <div>Portfolio</div>
+      <div>O mnie</div>
+      <div>Kontakt</div>
+    </div>
+  </header>;
+};
+export default Header;
