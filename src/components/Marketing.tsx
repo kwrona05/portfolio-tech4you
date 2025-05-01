d3f1b3632140e6626f21943beb757bea99e273f2
kwrona05 <kwrona05@gmail.com>
2025-05-01 15:08:22 +0200

    Component with MarketingSlogan

---
+++ src/components/Marketing.tsx
@@ -0,0 +1,9 @@
+const MarketingSlogan = () => {
+  return (
+    <div>
+      <h2 className="slide-up">Wnieś swoją firmę na wyzszy poziom!</h2>
+    </div>
+  );
+};
+
+export default MarketingSlogan;
